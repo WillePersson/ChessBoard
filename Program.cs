dcdb2b6f80f0f035b3d00ef969d3d6f4846d33a1
wille <144245981+WillePersson@users.noreply.github.com>
2023-09-18 11:14:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace ChessBoard
+{ 
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            string e1 = "◻";
+            string e2 = "◼";
+            Console.WriteLine("hur stor brädda? ");
+            
+            int number = Int32.Parse(Console.ReadLine());
+
+            int rows = number;
+            int columns = number;
+            
+            
+
+            
+
+            for(int i = 0; i < rows; i++)
+            {
+                
+                for (int j = 0; j < columns; j++) 
+                {
+                    if ((j + i) % 2 == 0) 
+                    {
+                        Console.Write(e1);
+                        Console.Write(" ");
+
+
+                    }
+                    else 
+                    {
+                        Console.Write(e2);
+                        Console.Write(" ");
+
+                    }    
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+            
+
+
+
+
+        }
+    }
+}
